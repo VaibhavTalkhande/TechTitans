cfcb22b2c023f012d628e6680a9cd6e8d3bf4189
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2024-02-16 12:48:34 +0530

    backend completed

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const courseRoutes = require('./routes/courseRoutes');
 const cors = require('cors');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const pocess = require('dotenv');
+pocess.config();
+const secret = process.env.JWT_SECRET;
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -10,7 +16,27 @@ app.use(express.json());
 app.use(cors({
     origin: '*'
 }));
-
+app.use('/api/courses', (req, res, next) => {
+    // Get token from header
+    const token = req.headers.authorization;
+  
+    // Check if token exists
+    if (!token) {
+      return res.status(401).json({ message: 'No token, authorization denied' });
+    }
+  
+    try {
+      // Verify token
+      const decoded = jwt.verify(token, 'secret');
+  
+      // Set user from token payload
+      req.user = decoded.user;
+      next();
+    } catch (err) {
+      res.status(401).json({ message: 'Invalid token' });
+    }
+  });
+  
 // Routes
 app.get('/', (req, res) => {
     console.log("hell from express")
@@ -21,6 +47,8 @@ app.get('/', (req, res) => {
 app.get('/api/example', (req, res) => {
     res.json({ message: 'This is an example API route' });
 });
+app.use('/api/users', userRoutes);
+app.use('/api/courses', courseRoutes);
 
 // Start server
 app.listen(PORT, () => {
